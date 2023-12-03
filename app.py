ca72e82f5fd256651e01c30d25e3f2269d63a481
EC2 Default User <ec2-user@ip-172-31-30-183.us-east-2.compute.internal>
2023-12-03 19:28:55 +0000

    Added exception handling to app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 from langchain_service import LangChainService
 import os
@@ -10,10 +9,20 @@ service = LangChainService('Chinook.db', os.environ['OPENAI_API_KEY'])
 
 @app.route('/query', methods=['POST'])
 def query():
-    data = request.json
-    query = data.get('query')
-    response = service.query_database(query)
-    return jsonify({'response': response})
+    try:
+        data = request.json
+        query_text = data.get('query')
+        if not query_text:
+            return jsonify({'error': 'No query provided'}), 400
+
+        response = service.query_database(query_text)
+        return jsonify({'response': response})
+
+    except Exception as e:
+        # print(e)
+
+        # Return a JSON response with the error message and a server error status code
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
